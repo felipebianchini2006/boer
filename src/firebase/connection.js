d18e8e1765074c8e7c9005ff3012ca3db26845ad
zun666 <kickbuttwovisk@gmail.com>
2025-04-29 15:47:07 -0300

    alteração

---
+++ src/firebase/connection.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDlvZtdvHCCyGGwMMZPEmjpE_0b8_nzZT0",
+  authDomain: "projeto-ddd8a.firebaseapp.com",
+  projectId: "projeto-ddd8a",
+  storageBucket: "projeto-ddd8a.firebasestorage.app",
+  messagingSenderId: "175715420871",
+  appId: "1:175715420871:web:dcb7a5c062d77a90f9cce2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
